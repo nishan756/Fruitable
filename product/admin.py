a41d6b87b4590210731d5c58f6f0f50fb138217c
Nishan <aanishan339@gmail.com>
2025-10-20 16:40:06 +0600

    Register model

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import  Category , Product
+# Register your models here
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name" , 'price' , "stock" , "unit" , "is_active" , "date"]
+
+@admin.register(Category)
+class CatAdmin(admin.ModelAdmin):
+    list_display = ["name" , 'is_active' , 'date' ]

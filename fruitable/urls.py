1a1c8a710a858978c8164b7f9e70b16d37ac9877
Nishan <aanishan339@gmail.com>
2025-10-20 16:55:46 +0600

    initial commit

---
+++ fruitable/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path , include
+from product.views import Home
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/', Home , name = 'home'),
+    path('product/', include('product.urls')),
+    path('session/', include('session.urls')),
+    path('summernote/', include('django_summernote.urls')),
+]
--- product/views.py
+++ product/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , get_object_or_404
+from .models import Product
+from django.core.paginator import Paginator
 # Create your views here.
 
 def Home(request):
-    print(request.cart)
     return render(request , 'index.html')
 
 def Shop(request):
-    return render(request , 'shop.html')
+    products = Product.objects.all()
+    paginator = Paginator(object_list = products , per_page = 20)
+    page = request.GET.get("page")
+    products = paginator.get_page(page)
+    return render(request , 'shop.html' , {"products":products})
+
+def ProductDetail(request , id):
+    product = get_object_or_404(Product , id = id)
+    return render(request , 'product.html' , {"product":product})

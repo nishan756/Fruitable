2a98a9040e76a18fbd74f0f196b0583b2fdba8f5
Nishan <aanishan339@gmail.com>
2025-10-20 16:43:34 +0600

    Changes some logic in function

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -2,9 +2,9 @@ from product.models import Category
 from .models import Cart , CartItem
 
 def CategoryList(request):
-    categories = Category.objects.all()
+    categories = Category.objects.all(is_active = True)
     return {"categories":categories}
 
 def TotalItem(request):
-    total = CartItem.objects.filter(cart = request.cart).count()
+    total = CartItem.objects.filter(cart = request.cart , is_ordered = False).count()
     return {"total":total}

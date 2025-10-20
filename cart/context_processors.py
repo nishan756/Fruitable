c8eec3a9829af56e8a4b8d5e5a76e187232e9957
Nishan <aanishan339@gmail.com>
2025-10-20 16:45:42 +0600

    Changes logic in function

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -2,7 +2,7 @@ from product.models import Category
 from .models import Cart , CartItem
 
 def CategoryList(request):
-    categories = Category.objects.all(is_active = True)
+    categories = Category.objects.filter(is_active = True)
     return {"categories":categories}
 
 def TotalItem(request):

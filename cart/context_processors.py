beb93feb12ce70fc16db8a86cfc52dc8440a79bc
Nishan <aanishan339@gmail.com>
2025-10-20 15:49:39 +0600

    Added total cart item method

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -4,3 +4,7 @@ from .models import Cart , CartItem
 def CategoryList(request):
     categories = Category.objects.all()
     return {"categories":categories}
+
+def TotalItem(request):
+    total = CartItem.objects.filter(cart = request.cart).count()
+    return {"total":total}

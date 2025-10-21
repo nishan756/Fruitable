86fdc84134b74ce2b59ba32c38e25c2fc547ba04
Nishan <aanishan339@gmail.com>
2025-10-22 01:20:40 +0600

    addd product review urls

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,9 @@
-from .views import Shop
+from .views import Shop , ProductDetail , AddReview
 from django.urls import path
 
 urlpatterns = [
     path('shop/' , Shop , name = 'shop'),
+    path('detail/<str:id>/' , ProductDetail , name = 'product-detail'),
+
+    path('review/<str:id>/' , AddReview , name = 'add-review'),
 ]

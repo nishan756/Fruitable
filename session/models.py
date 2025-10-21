c2288acf04e13a97c81644a54f8052c79d377b5d
Nishan <aanishan339@gmail.com>
2025-10-21 23:46:15 +0600

    Added image field in CustomUser model

--- session/models.py
+++ session/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from cloudinary.models import CloudinaryField
 # Create your models here.
 
 
 class CustomUser(AbstractUser):
     username = models.CharField(max_length = 150 , blank = True , null = True , unique = True)
+    image = CloudinaryField("image" , folder = "fruitable/user_image" , blank = True , null = True)
     email = models.EmailField(unique = True , blank = True , null = True)
     phone = models.CharField(max_length = 11 , blank = True , null = True , unique = True)
 

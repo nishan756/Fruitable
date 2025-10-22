4efbf91e469ac0ba88f8b95bad989016b6ab5c83
Nishan <aanishan339@gmail.com>
2025-10-22 10:55:34 +0600

    Registered cart and cart item model

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Cart , CartItem
 # Register your models here.
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ["user"]
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ["cart" , "product" , "qty" , "price" , "is_ordered"]

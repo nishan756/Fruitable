26ed80a40eacdbbf648a1125c4371ba3dbb42f3e
Nishan <aanishan339@gmail.com>
2025-10-25 01:30:22 +0600

    RequestProduct form made

--- product/forms.py
+++ product/forms.py
@@ -1,4 +1,4 @@
-from .models import ProductReview
+from .models import ProductReview , ProductRequest
 from django import forms 
 
 class ReviewForm(forms.ModelForm):
@@ -6,3 +6,21 @@ class ReviewForm(forms.ModelForm):
         model = ProductReview
         fields = ["point" , "review"]
 
+class RequestForm(forms.ModelForm):
+    class Meta:
+        model = ProductRequest
+        exclude = ["user","date","product"]
+        widgets = {
+            'name':forms.TextInput(
+                attrs = {
+                    'type':'text',
+                    "class":"form-control"
+                }
+            ),
+            'email':forms.EmailInput(
+                attrs = {
+                    'type':'text',
+                    "class":"form-control"
+                }
+            )
+        }

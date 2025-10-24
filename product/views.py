087f613b2fa7de918caa9ecbcaeb485917301308
Nishan <aanishan339@gmail.com>
2025-10-25 01:29:56 +0600

    Request function implemented

--- product/views.py
+++ product/views.py
@@ -4,14 +4,16 @@ from django.core.paginator import Paginator
 from functools import wraps
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import ReviewForm
+from .forms import ReviewForm , RequestForm
+from django.core.mail import send_mail
+from session.views import get_user
 # Create your views here.
 
 def ProductRequired(func):
     @wraps(func)
     def wrapper(request , id , *args , **kwargs):
         try:
-            product = Product.objects.get(id = id , is_active = True)
+            product = Product.objects.get(id = id)
         except Product.DoesNotExist:
             return HttpResponse(content = "Product not found")
         return func(request , id , product , *args , **kwargs)
@@ -21,7 +23,7 @@ def Home(request):
     return render(request , 'index.html')
 
 def Shop(request):
-    products = Product.objects.filter(is_active = True)
+    products = Product.objects.all()
     paginator = Paginator(object_list = products , per_page = 12)
     page = int(request.GET.get("page" ,  1))
     products = paginator.get_page(page)
@@ -49,5 +51,37 @@ def AddReview(request , id , product):
             messages.error(request , 'Something went wrong')
 
     return redirect(REFERER)
+
+@ProductRequired
+def Request(request , id , product):
+    user = get_user(request)
+    if product.stock > 0:
+        messages.info(request , "The product is in stock")
+        return redirect("product-detail",product.id)
+    if request.method == 'POST':
+        form = RequestForm(data = request.POST)
+        user = get_user(request)
+
+        if form.is_valid():
+            new_request = form.save(commit = False)
+            if user:
+                new_request.user = user
+            else:
+                name = form.cleaned_data.get("name")
+                email = form.cleaned_data.get("email")
+                new_request.name = name
+                new_request.email = email
+            new_request.product = product
+            new_request.save()
+            messages.success(request , "Thanks for request us")
+            return redirect("shop")
+
+    else:
+        form = RequestForm(initial = {"name":request.user.get_full_name(),"email":request.user.email} if request.user.is_authenticated else None)
+    return render(request , "request.html" , {"form":form , "product":product})
+
+
+
+
     
 

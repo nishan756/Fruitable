d7217178d5d623b870d31b0cc113413e131f8353
Nishan <aanishan339@gmail.com>
2025-10-25 20:37:41 +0600

    ConfirmOrder model registered

--- cart/admin.py
+++ cart/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Cart , CartItem
+from .models import Cart , CartItem , ConfirmOrder
 # Register your models here.
 
 @admin.register(Cart)
@@ -9,3 +9,7 @@ class CartAdmin(admin.ModelAdmin):
 @admin.register(CartItem)
 class CartItemAdmin(admin.ModelAdmin):
     list_display = ["cart" , "product" , "qty" , "price" , "is_ordered"]
+
+@admin.register(ConfirmOrder)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["name","phone","email","location","shipping_charge","price","status"]

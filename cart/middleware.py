ffd1210e7a65537c9eae45b40499913df23b4028
Nishan <aanishan339@gmail.com>
2025-10-25 11:35:52 +0600

    Middelware bug issue fixes

--- cart/middleware.py
+++ cart/middleware.py
@@ -1,17 +1,17 @@
 from django.utils.deprecation import MiddlewareMixin
 from .models import Cart
 from django.db.models import Q
-from session.models import CustomUser
 
 class SessionKeyMiddleware(MiddlewareMixin):
-    def process_request(self , request):
+    def process_request(self, request):
+        cart = None
         if request.user.is_authenticated:
-            cart , created = Cart.objects.get_or_create(user__id = request.user.id)
+            cart , created = Cart.objects.get_or_create(user = request.user)
         else:
-            session_key = request.session.get("session_key")
+            session_key = request.session.session_key
             if not session_key:
                 request.session.create()
                 session_key = request.session.session_key
-            cart = Cart.objects.create(session_key = session_key)
+            cart , created = Cart.objects.get_or_create(session_key = session_key)
+        
         request.cart = cart
-    
